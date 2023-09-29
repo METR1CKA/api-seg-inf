6fd1394a7616f848712a10995af07fcb69d7e05a
METR1CKA <ferchosalazar054@gmail.com>
2023-09-28 23:48:20 -0600

    [UPD] Version 1.4
    - Complete websockets
    - Add model
    - Add controller
    - Add json for salas

--- app/Services/Ws.ts
+++ app/Services/Ws.ts
@@ -2,16 +2,33 @@ import { Server } from 'socket.io'
 import AdonisServer from '@ioc:Adonis/Core/Server'
 
 class Ws {
+  /* `public io: Server` is declaring a public property `io` of type `Server`. This property will be
+  used to store an instance of the `Server` class from the `socket.io` library. */
   public io: Server
+  /* The line `private booted: boolean = false` is declaring a private property `booted` of type
+  `boolean` and initializing it with the value `false`. This property is used to keep track of
+  whether the `boot()` method has been called or not. */
   private booted: boolean = false
+  /* The line `private instance = AdonisServer.instance!` is declaring a private property `instance`
+  and initializing it with the value of `AdonisServer.instance`. The `!` symbol is a non-null
+  assertion operator in TypeScript, which tells the compiler that the value of
+  `AdonisServer.instance` will not be null or undefined. */
   private instance = AdonisServer.instance!
+  /* The line `private configs = { cors: { origin: true } }` is declaring a private property `configs`
+  and initializing it with an object. This object has a property `cors` which itself is an object
+  with a property `origin` set to `true`. */
   private configs = {
     cors: {
       origin: true
     }
   }
 
-  public boot() {
+  /**
+   * The function initializes a server instance if it has not already been initialized.
+   * @returns If the `booted` flag is already set to `true`, then nothing is being returned. The
+   * function will exit early and no value will be returned.
+   */
+  public boot(): void {
     const { booted, instance, configs } = this
 
     if (booted) return
--- start/socket.ts
+++ start/socket.ts
@@ -1,3 +1,4 @@
+import SalasController from 'App/Controller/SalasController'
 import Ws from 'App/Services/Ws'
 
 Ws.boot()
@@ -5,9 +6,113 @@ Ws.boot()
 const { io } = Ws
 
 io.on('connection', socket => {
+  console.log('\nNueva conexion:', socket.id)
+
+  console.log('\nMandando salas')
+  socket.emit('get:salas', SalasController.getSalas())
+
+  socket.on('send:message', data => {
+    console.log('\nMessage:', data)
+
+    socket.broadcast.emit('get:messages', data)
+  })
+
+  socket.on('send:sala', sala => {
+    console.log('\nSala:', { sala, tsala: typeof sala })
+
+    sala = typeof sala == 'string'
+      ? JSON.parse(sala)
+      : sala
+
+    SalasController.createSala({ sala })
+
+    const salas = SalasController.getSalas()
+
+    console.log('\nSala añadida, salas:', salas)
+
+    io.emit('get:salas', salas)
+  })
+})
+
+// Using Sebas logic
+/* import Ws from 'App/Services/Ws'
+
+Ws.boot()
+
+const { io } = Ws
+
+class Sala {
+  nombre: string;
+  password: string;
+
+  constructor(nombre: string, password: string) {
+    this.nombre = nombre;
+    this.password = password;
+  }
+
+
+  static fromJson(json: { nombre: string, password: string }): Sala {
+    console.log("Sala Json")
+    console.log(json)
+
+    return new Sala(json["nombre"], json["password"]);
+  }
+
+}
+
+const nuevaSala = new Sala("Sala dd", "3j1Dyq0r08TwYi/GDe7NBrNKDyWoDjC4H+BRo8VHmbs=");
+const nuevaSala1 = new Sala("memnnas", "3j1Dyq0r08TwYi/GDe7NBrNKDyWoDjC4H+BRo8VHmbs=");
+const nuevaSala2 = new Sala("Salaasasas", "3j1Dyq0r08TwYi/GDe7NBrNKDyWoDjC4H+BRo8VHmbs=");
+
+const salas = [nuevaSala, nuevaSala1, nuevaSala2];
+
+//List variable of dicts(nameSala, pass)
+
+
+
+io.on('connection', socket => {
+  console.log('new connection')
+  console.log("Mandando salas")
+  socket.emit('get:salas', salas)
+
+
+
+
   // All sockets except the current socket retrieve messages
   socket.on('send:message', data => {
+
+
     console.log(data)
     socket.broadcast.emit('get:messages', data)
   })
-})
+
+  socket.on('send:sala', (data) => {
+    console.log("Sala recibida")
+
+    // console.log(data)
+    // console.log(typeof data)
+
+    try {
+      // Intenta analizar la cadena JSON
+      data = JSON.parse(data)
+
+      // console.log(data)
+      // console.log(typeof data)
+
+    } catch (error) {
+      console.error("Error al analizar JSON:", error)
+    }
+
+
+    // Verifica si los datos recibidos son válidos
+
+    const salaJson = Sala.fromJson(data)
+    //console.log(salaJson)
+    salas.push(salaJson)
+    console.log("Sala añadida")
+    // console.log(salas)
+    // Emite el array actualizado
+    io.emit('get:salas', salas)
+
+  })
+}) */
